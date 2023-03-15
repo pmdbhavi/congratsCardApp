7bf5d94ef2c816d29c29b984a7d0010285c47c21
pmdbhavi <srirenu640@gmail.com>
2023-03-15 10:47:10 +0000

    congrats

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="Container">
+    <h1 className="Heading">Congratulations</h1>
+    <div className="Con">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="para">Kiran V</h1>
+      <p className="para">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
